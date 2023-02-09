6229370db7cb0463d69e2c082c4ee0977b768d73
mpotane <1000100ten@gmail.com>
2023-02-09 20:13:10 +0800

    Add custom loading.

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,7 +1,12 @@
+import styles from "./Loading.module.css";
+
 export default function Loading() {
   return (
     <div className="min-h-screen grid place-items-center">
-      <button className="btn loading">loading</button>
+      <div className={styles.loader}>
+        <span>loading</span>
+        <span>loading</span>
+      </div>
     </div>
   );
 }
